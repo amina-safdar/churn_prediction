2d8a961bf926babcd724833ed99688658b80a941
Amina Safdar <58341986+amina-safdar@users.noreply.github.com>
2023-05-11 18:57:00 -0400

    Create table for logged events
    From liveBook users

---
+++ churn_prediction/data_generation/create_event.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS  churn.event (
+    account_id VARCHAR COLLATE pg_catalog."default" NOT NULL,
+    event_time timestamp(6) without time zone NOT NULL,
+    event_type VARCHAR COLLATE pg_catalog."default" NOT NULL,
+    product_id integer,
+    additional_data VARCHAR COLLATE pg_catalog."default")
+
+TABLESPACE pg_default;
+
+CREATE INDEX  IF NOT EXISTS  idx_account_id
+    ON churn.event USING btree
+    (account_id)
+    TABLESPACE pg_default;
+
+CREATE INDEX  IF NOT EXISTS  idx_account_time
+    ON churn.event USING btree
+    (account_id, event_time)
+    TABLESPACE pg_default;
+
+CREATE INDEX  IF NOT EXISTS  idx_event_time
+    ON churn.event USING btree
+    (event_time)
+    TABLESPACE pg_default;
+
+CREATE INDEX  IF NOT EXISTS  idx_event_type
+    ON churn.event USING btree
+    (event_type)
+    TABLESPACE pg_default;
+
+CREATE INDEX  IF NOT EXISTS  idx_product_id
+    ON churn.event USING btree
+    (product_id)
+    TABLESPACE pg_default;
