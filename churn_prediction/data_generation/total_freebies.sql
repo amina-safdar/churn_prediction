4bd70c6f352c8ed569d433e6f332d0904b302caa
Amina Safdar <58341986+amina-safdar@users.noreply.github.com>
2023-09-04 23:16:59 -0400

    Extract `total_freebies` feature

---
+++ churn_prediction/data_generation/total_freebies.sql
@@ -0,0 +1,16 @@
+WITH date_vals AS (
+	SELECT i::timestamp AS metric_date
+	FROM generate_series('2020-02-22', '2020-06-01', '28 day'::interval) i)
+
+INSERT INTO churn.metric (account_id, metric_time, metric_id, metric_value)	
+	SELECT 
+		account_id, 
+		metric_date, 
+		12,  
+		COUNT(*) AS metric_value
+    FROM churn.event JOIN date_vals
+	    ON event.event_time < metric_date 
+		    AND event.event_time >= metric_date - interval '84 days'
+    WHERE event_type IN ('ReadingFreePreview', 'FreeContentCheckout', 'ReadingOpenChapter')
+    GROUP BY account_id, metric_date
+    ON CONFLICT DO NOTHING;
