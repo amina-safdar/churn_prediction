0fb3b7dcc2964bb040a4e9605d2ce3608281d1e1
Amina Safdar <58341986+amina-safdar@users.noreply.github.com>
2023-09-04 23:10:19 -0400

    Extract `distinct_products` feature

---
+++ churn_prediction/data_generation/distinct_products.sql
@@ -0,0 +1,15 @@
+WITH date_vals AS (
+	SELECT i::timestamp AS metric_date
+	FROM generate_series('2020-02-22', '2020-06-01', '28 day'::interval) i)
+
+INSERT INTO churn.metric (account_id, metric_time, metric_id, metric_value)
+	SELECT 
+		account_id, 
+		metric_date, 
+		11,  
+		COUNT(DISTINCT product_id) AS metric_value
+    FROM churn.event JOIN date_vals
+	    ON event.event_time < metric_date 
+		    AND event.event_time >= metric_date - interval '84 days'
+    GROUP BY account_id, metric_date
+    ON CONFLICT DO NOTHING;
